29a22b231ea4c5a4d88773458e9f2ec6bdcf2369
Hablu Programmer <81285059+Hablu-Programmer@users.noreply.github.com>
2022-01-05 11:41:07 +0600

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb:1,
+        question:'What Does HTML Stand For?',
+        answer:'Hyper Text Markup Language',
+        options:[
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What Does CSS Stand For?",
+        answer: "Cascading Style Sheet",
+        options: [
+          "Common Style Sheet",
+          "Colorful Style Sheet",
+          "Computer Style Sheet",
+          "Cascading Style Sheet"
+        ]
+      },
+      {
+        numb: 3,
+        question: "What Does PHP Stand For?",
+        answer: "Hypertext Preprocessor",
+        options: [
+          "Hypertext Preprocessor",
+          "Hypertext Programming",
+          "Hypertext Preprogramming",
+          "Hometext Preprocessor"
+        ]
+      },
+    
+      {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+          "Stylish Question Language",
+          "Stylesheet Query Language",
+          "Statement Question Language",
+          "Structured Query Language"
+        ]
+      },
+
+      {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+]
